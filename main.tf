266c61b5071770a6bf55a9d511db979d22960b32
Ibrahim Ozbekler <39748824+ibrahim-ozbek@users.noreply.github.com>
2024-04-25 11:48:33 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,89 +5,81 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+
 }
 
 provider "aws" {
   region = "us-east-1"
 }
 
-
-variable "names" {
-  type    = list(string)
-  default = ["nodejs_instance", "postgresql_instance", "react_instance"]
+variable "tags" {
+  default = ["postgresql", "nodejs", "react"]
 }
 
-
-
-resource "aws_instance" "worker_nodes" {
-  ami                  = "ami-0f095f89ae15be883"
-  instance_type        = "t2.micro"
+resource "aws_instance" "managed_nodes" {
+  ami = "ami-0f095f89ae15be883"
+  count = 3
+  instance_type = "t2.micro"
+  key_name = "son"
+  vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
   iam_instance_profile = "jenkins-project-profile"
-  vpc_security_group_ids = [aws_security_group.instances_sg.id]
-  count                = 3
-
   tags = {
-    Name = "${element(var.names, count.index)}"
+    Name = "ansible_${element(var.tags, count.index )}"
+    stack = "ansible_project"
+    environment = "development"
   }
 }
 
-# Security group for all instances
-resource "aws_security_group" "instances_sg" {
-  name        = "instances_sg"
-  description = "Security group for the web application instances"
+resource "aws_security_group" "tf-sec-gr" {
+  name = "ansible-sec-gr"
+  tags = {
+    Name = "ansible-sec-gr"
+  }
 
-  # Ingress rules
   ingress {
     from_port   = 22
-    to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # SSH access from anywhere
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 5000
+    protocol    = "tcp"
     to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Node.js port access from anywhere
-  }
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # PostgreSQL port access from anywhere
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 3000
-    to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # React port access from anywhere
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5432
+    protocol    = "tcp"
+    to_port     = 5432
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # React port access from anywhere
-  }
-  # Egress rules
   egress {
     from_port   = 0
+    protocol    = -1
     to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-# Outputs for IP addresses of each application server
-output "node_public_ip" {
-  value = aws_instance.worker_nodes[0].public_ip
-}
-
-output "postgres_private_ip" {
-  value = aws_instance.worker_nodes[1].private_ip
 }
 
 output "react_ip" {
-  value = aws_instance.worker_nodes[2].public_ip
+  value = "http://${aws_instance.managed_nodes[2].public_ip}:3000"
+}
+
+output "node_public_ip" {
+  value = aws_instance.managed_nodes[1].public_ip
+
+}
+
+output "postgre_private_ip" {
+  value = aws_instance.managed_nodes[0].private_ip
+
 }
